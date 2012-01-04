68e2e2e556754b1927ca8981abf35b8c51419870
Matt Elliott <melliott@ccistudios.com>
2012-01-04 11:54:31 -0500

    index html created

--- template/templates/conceptConstruction/index.php
+++ template/templates/conceptConstruction/index.php
@@ -54,26 +54,33 @@ $analytics = "UA-XXXXX-X"; // FIXME Update to client ID
 
 <body class="<?= $menu ?>">
 
-	<div id="wrapper">
-		<header>
-			<jdoc:include type="modules" name ="header" style="xhtml" />
-		</header>
-
-		<div id="main">
-			<aside>
-				<jdoc:include type="modules" name="left" style="xhtml" />
-			</aside>
-
-			<article>
-				<jdoc:include type="component" />
-			</article>
-		</div>
-
-		<footer>
-			<jdoc:include type="modules" name="footer" style="xhtml" />
-		</footer>
+	<div id="header">
+		<jdoc:include type="modules" name="header" style="xhtml" />
 	</div>
-
+	
+	<div id="body">
+		<div id="bodyTop">
+			<jdoc:include type="modules" name="bodyTop" style="xhtml" />
+		</div>
+		<div id="content">
+			<jdoc:include type="component" />
+		</div>
+		<div id="sidebar">
+			<jdoc:include type="modules" name="sidebar" style="xhtml" />
+		</div>
+		<div id="bottom">
+			<jdoc:include type="modules" name="bottom" style="xhtml" />
+		</div>
+	</div>
+	
+	<div id="footer">
+		<div id="copyright">
+			
+		</div>
+		<jdoc:include type="modules" name="footer" style="xhtml" />
+	</div>
+	
+	
 	<div class="hidden">
 		<jdoc:include type="modules" name="hidden" style="raw" />
 	</div>
