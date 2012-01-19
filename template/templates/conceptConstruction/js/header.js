ed72b5714f3d97693fcb7cea199b1cac8deb3239
Matt Elliott <melliott@ccistudios.com>
2012-01-19 11:25:03 -0500

    cleaned out whitespace in header.js

--- template/templates/conceptConstruction/js/header.js
+++ template/templates/conceptConstruction/js/header.js
@@ -5,7 +5,7 @@ window.addEvent('domready', function () {
 	if (Modernizr.csstransforms) {
 		buttons.setStyle('background-image', 'url(/templates/conceptConstruction/images/close.png)');
 	}
-		
+	
 	buttons.addEvent('click', function() {
 		if (header.hasClass('close')) {
 			header.tween('height', '390');
@@ -14,7 +14,6 @@ window.addEvent('domready', function () {
 			header.tween('height', '215');
 			Cookie.write('windowOpen', 0);
 		}
-
 		header.toggleClass('close');
 	});
 	
@@ -23,7 +22,6 @@ window.addEvent('domready', function () {
 	}
 	
 	console.log(Cookie.read('windowOpen'));
-	
-	
+
 });
 
